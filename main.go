5fc621ba0c2ef9f27063c7224a93891014ccfcd2
Bogdan <bogdanshibilov@gmail.com>
2024-06-11 17:48:29 +0500

    completed task

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"errors"
 	"flag"
-	"fmt"
-	"io"
-	"io/fs"
 	"log"
-	"os"
 )
 
 var (
@@ -31,53 +26,9 @@ func main() {
 		log.Panic(err)
 	}
 
-	sourceFile := readFile()
-	defer func(sourceFile *os.File) {
-		_ = sourceFile.Close()
-	}(sourceFile)
-
-	fi, _ := sourceFile.Stat()
-	if offset > fi.Size() {
-		return
-	}
-	if offset+limit > fi.Size() {
-		limit = fi.Size() - offset
-	}
-
-	destFile := createDestFile()
-	defer func(destFile *os.File) {
-		_ = destFile.Close()
-	}(destFile)
-
-	offset, err = sourceFile.Seek(offset, 0)
+	err = CopyFile(from, to, offset, limit)
 	if err != nil {
 		log.Panic(err)
-	}
-
-	reader := io.LimitReader(sourceFile, limit)
-
-	var bufSize int
-	if limit > 256 {
-		bufSize = 256
-	} else {
-		bufSize = int(limit)
-	}
-	buf := make([]byte, bufSize)
-	var readProgress int
-	for {
-		r, err := reader.Read(buf)
-		readProgress += r
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			log.Panic(err)
-		}
-		_, err = destFile.Write(buf)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("Read %d bytes out of %d\n", readProgress, limit)
 	}
 }
 
@@ -89,32 +40,4 @@ func validateFlags() error {
 		return ErrToValueNotSpecified
 	}
 	return nil
-}
-
-func readFile() *os.File {
-	sourceFile, err := os.Open(from)
-	if err != nil {
-		if errors.Is(err, fs.ErrNotExist) {
-			log.Panicf("Source file %s does not exist\n", from)
-		}
-		if errors.Is(err, fs.ErrPermission) {
-			log.Panicf("Permission denied to read file %s\n", from)
-		}
-		log.Panic(err)
-	}
-	return sourceFile
-}
-
-func createDestFile() *os.File {
-	destFile, err := os.OpenFile(to, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		if errors.Is(err, fs.ErrExist) {
-			log.Panicf("Destination file %s already exists\n", to)
-		}
-		if errors.Is(err, fs.ErrPermission) {
-			log.Panicf("Permission denied to write file %s\n", to)
-		}
-		log.Panic(err)
-	}
-	return destFile
 }
